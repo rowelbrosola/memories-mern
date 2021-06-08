ea26b1c9c486d8a799956f2c32e6d8898dea9672
Rowel Brosola <rowel.licagroup@gmail.com>
2021-06-08 23:48:00 +0800

    update and delete functions

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -1,13 +1,13 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
 import FileBase from 'react-file-base64';
 
 import useStyles from './styles';
-import { createPost } from '../../actions/posts';
+import { createPost, updatePost } from '../../actions/posts';
 
-const Form = () => {
+const Form = ({ currentId, setCurrentId }) => {
     const [postData, setPostData] = useState({
         creator: '',
         title: '',
@@ -15,22 +15,41 @@ const Form = () => {
         tags: '',
         selectedFile: ''
     });
-    const dispatch = useDispatch()
+    const post = useSelector((state) => currentId ? state.posts.find((p) => p._id === currentId) : null);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        if (post) setPostData(post)
+    }, [post]);
+
     const classes = useStyles();
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        dispatch(createPost(postData));
+
+        if (currentId) {
+            dispatch(updatePost(currentId, postData))
+        } else {
+            dispatch(createPost(postData));
+        }
+        clear();
     }
 
     const clear = () => {
-
+        setCurrentId(null);
+        setPostData({
+            creator: '',
+            title: '',
+            message: '',
+            tags: '',
+            selectedFile: ''
+        });
     }
 
     return (
         <Paper className={classes.paper}>
             <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
-                <Typography variant="h6">Creating a Memory</Typography>
+                <Typography variant="h6">{currentId ? 'Editing' : 'Creating a Memory'}</Typography>
                 <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value })} />
                 <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })} />
                 <TextField name="message" variant="outlined" label="Message" fullWidth value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })} />
--- client/src/components/Posts/Post/index.js
+++ client/src/components/Posts/Post/index.js
@@ -1,11 +1,51 @@
 import React from 'react';
-// import useStyles from './styles';
-const Post = () => {
-    // const classes = useStyles();
+import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
+import ThumpUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import DeleteIcon from '@material-ui/icons/Delete';
+import MoreHorizon from '@material-ui/icons/MoreHoriz';
+
+import moment from 'moment';
+
+import useStyles from './styles';
+import { useDispatch } from 'react-redux';
+
+import { deletePost } from '../../../actions/posts';
+
+const Post = ({ post, setCurrentId }) => {
+    const classes = useStyles();
+    const dispatch = useDispatch();
+
     return (
-        <div>
-            Post
-        </div>
+        <Card className={classes.card}>
+            <CardMedia className={classes.media} image={post.selectedFile} title={post.title} />
+            <div className={classes.overlay}>
+                <Typography variant="h6">{post.creator}</Typography>
+                <Typography variant="body2">{moment(post.created_at).fromNow()}</Typography>
+            </div>
+            <div className={classes.overlay2}>
+                <Button style={{ color: 'white' }} size="small" onClick={() => { setCurrentId(post._id) }}>
+                    <MoreHorizon fontSize="default" />
+                </Button>
+            </div>
+            <div className={classes.details}>
+                <Typography variant="body2" color="textSecondary">{post.tags.map((tag) => `#${tag} `)}</Typography>
+            </div>
+            <Typography className={classes.title} variant="h5" gutterBottom>{post.title}</Typography>
+            <CardContent>
+                <Typography variant="h5" gutterBottom>{post.message}</Typography>
+            </CardContent>
+            <CardActions className={classes.cardActions}>
+                <Button size="small" color="primary" onClick={() => { }}>
+                    <ThumpUpAltIcon fontSize="small" />
+                    Like
+                    {post.likeCount}
+                </Button>
+                <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}>
+                    <DeleteIcon fontSize="small" />
+                    Delete
+                </Button>
+            </CardActions>
+        </Card>
     )
 }
 
